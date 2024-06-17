2fa9e4d87b3e01e09f7130e3faed1cd7938cfcdf
Ella <emmanuella.johnson@stu.ocsb.ca>
2024-06-17 09:52:19 -0400

    almost done

--- pages/script.js
+++ pages/script.js
@@ -1,19 +1,26 @@
-//event listeners
-const img = document.getElementById('images');
+document.getElementById('initialImage').addEventListener('click', function() {
+  // Create a new image element
+  var newImage = document.createElement('img');
+  newImage.src = "../step_2/ball.png"; // Path to the new image
+  newImage.width = 300;
 
-let toggle = true;
+  // Append the new image to the container
+  document.getElementById('newImageContainer').appendChild(newImage);
+});
 
-img.addEventListener ("click", toggle_picture)
-
-
-function toggle_picture() {
-  //toggle
-  toggle = !toggle;
-  if (toggle) {
-    img.src= "";
-  }else {
-    img.src = "../image.ball.png";
-  }
-img.src  = "../images.ball.png";
+var myVariable = 0
+function Clearbutton(){
+  myVariable= 0;
+  document.getElementById('display').textContent = "Variable Value " + myVariable;
+  document.getElementById('myButton').addEventListener('click' , Clearbutton)
 }
 
+document.getElementById('ThirdImage').addEventListener('click', function() {
+  // Create a new image element
+  var ThirdImage = document.createElement('img');
+  ThirdImage.src = "../step_2/ball.png"; // Path to the new image
+  ThirdImage.width = 300;
+
+  // Append the new image to the container
+  document.getElementById('ThirdImageContainer').appendChild(ThirdImage);
+});
